3ec8ae033e40ca0ec795c95d0380d8fbcebcc96a
mkissarli <kissarlim@gmail.com>
2020-08-31 22:02:15 +0100

    Bunch of gravity, bunch of drawing

--- src/main.c
+++ src/main.c
@@ -10,10 +10,6 @@ UINT8 create_sprite_num(){
     ++total_sprites;
     return total_sprites;
 }
-static UINT8 GRAVITY = 9;
-void gravity(){
-    
-}
 
 typedef struct MSprite
 {
@@ -23,7 +19,13 @@ typedef struct MSprite
     UINT8 max_animations;
     UINT8 sprite_number;
     UINT8 speed;
-} MSprite;   
+} MSprite;
+
+MSprite* sprites[40];
+
+void draw(MSprite* sprite){
+    move_sprite(sprite->sprite_number, sprite->x, sprite->y);
+}
 
 typedef struct MPlayer
 {
@@ -41,18 +43,25 @@ void next_animation(MSprite* sprite){
     set_sprite_tile(sprite->sprite_number, sprite->animation_index);
 }
 
+static UINT8 GRAVITY = 9;
+UINT8 gravity_time = 0;
+void gravity(MSprite* sprite){
+    //Collision Check
+    sprite->y += 1;
+}
+
 void move_player(MPlayer* player, UINT8 move, bool is_x){
     player->moved = true;
     if(is_x == true){
-        player->sprite.x += move;
-        scroll_sprite(player->sprite.sprite_number, move, 0);        
+        player->sprite.x += move;       
     }
     else {
         player->sprite.y += move;
-        scroll_sprite(player->sprite.sprite_number, 0, move);
     }
     next_animation(&(player->sprite));
+    //draw(&(player->sprite));
 }
+
 
 void check_collision_with_wall(){
     
@@ -90,8 +99,8 @@ void main()
 {
     UPDATE_JOYPAD_STATE;
     
-    MPlayer player = {{88, 78, 0, 3, 0, 5}, false, 60, 0};
-
+    MPlayer player = {{88, 78, 0, 3, 0, 5}, false, 6, 0};
+    sprites[0] = &(player.sprite);
     set_sprite_data(player.sprite.sprite_number, player.sprite.max_animations, MainChar);
     set_sprite_tile(player.sprite.sprite_number, player.sprite.animation_index);
     move_sprite(player.sprite.sprite_number, player.sprite.x, player.sprite.y);
@@ -99,6 +108,21 @@ void main()
     UINT8 movement = 0;
 
     while(1){
+        //Update loop
         player_movement(&player);
+        /*++gravity_time;
+        for(UINT8 i = 0; i < 40; ++i){
+            if(gravity_time > 6) { gravity(sprites[i]); }
+        }
+        gravity_time = 0;
+        */
+        //Draw loop
+        for(UINT8 i = 0; i < 40; ++i){
+            draw(sprites[i]);
+        //    move_sprite(0, sprites[i]->x, sprites[i]->y);
+        }
+        
+        //draw(sprites[0]);
+        wait_vbl_done();
     }
 }
