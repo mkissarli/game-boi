a8c438a4366f2af8f1876c3748ec784937b17556
mkissarli <kissarlim@gmail.com>
2020-08-31 20:45:09 +0100

    More generalized input and player system.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <stdbool.h> 
 #include "../sprites/char.c"
-
-//UINT8[] sprites = UINT8[];
+#include "../src/input.c"
 
 void gravity(){
     
@@ -10,68 +10,74 @@ void gravity(){
 
 UINT8 total_sprites = 0;
 
-typedef struct MSprite {
+typedef struct MSprite
+{
     UINT8 x;
     UINT8 y;
     UINT8 animation_index;
     UINT8 max_animations;
     UINT8 sprite_number;
+    UINT8 speed;
 } MSprite;   
 
-/*void next_sprite(MSprite* s){
-    ++(*s.animation_index);
-    if(*s.animation_index == *s.max_animations){
-        s.animation_index = 0;
+typedef struct MPlayer
+{
+    MSprite sprite;
+    bool    moved;
+    UINT8   movement_delay;
+    UINT16   movement_time;
+} MPlayer;
+
+void move_player(MPlayer* player, UINT8 move, bool is_x){
+    player->moved = true;
+    if(is_x == true){
+        player->sprite.x += move;
+        scroll_sprite(player->sprite.sprite_number, move, 0);        
     }
-    set_sprite_tile(*s.sprite_number, *s.animation_index);        
+    else {
+        player->sprite.y += move;
+        scroll_sprite(player->sprite.sprite_number, 0, move);
+    }
 }
-/*
-void next_sprite(UINT8 sprite_index, UINT8* current_sprite_index){
-    ++(*current_sprite_index);
-    if(*current_sprite_index == 3){
-        *current_sprite_index = 0;
+void player_movement(MPlayer* player)
+{
+    if(!(player->moved)){
+        if(JOYPAD_DOWN_PAD_L){
+            move_player(player, -player->sprite.speed, true);
+        }
+        else if(JOYPAD_DOWN_PAD_R){
+            move_player(player, player->sprite.speed, true);
+        }
+        if(JOYPAD_DOWN_PAD_U){
+            move_player(player, -player->sprite.speed, false);
+        }
+        else if(JOYPAD_DOWN_PAD_D){
+            move_player(player, player->sprite.speed, false);
+        }
     }
+    if(player->movement_time > player->movement_delay){
+        player->movement_time = 0;
+        player->moved = false;
+    }
+
+    ++(player->movement_time);
     
-    set_sprite_tile(sprite_index, *current_sprite_index);
 }
-*/
+
 void main()
 {
-    MSprite main_char = {88, 78, 0, 3, 0};
+    UPDATE_JOYPAD_STATE;
+    
+    MPlayer player = {{88, 78, 0, 3, 0, 10}, false, 60, 0};
     UINT8 current_sprite_index = 0;
-//    UINT8 main_char = 1;
-    set_sprite_data(main_char.sprite_number, main_char.max_animations, MainChar);
-    set_sprite_tile(main_char.sprite_number, main_char.animation_index);
-    move_sprite(main_char.sprite_number, main_char.x, main_char.y);
+    
+    set_sprite_data(player.sprite.sprite_number, player.sprite.max_animations, MainChar);
+    set_sprite_tile(player.sprite.sprite_number, player.sprite.animation_index);
+    move_sprite(player.sprite.sprite_number, player.sprite.x, player.sprite.y);
     SHOW_SPRITES;
     UINT8 movement = 0;
 
     while(1){
-        switch(joypad()){
-        case J_LEFT:
-            scroll_sprite(main_char.sprite_number, -10, 0);
-            //next_sprite(main_char, &current_sprite_index);
-            movement = 1;
-            delay(100);
-            break;
-        case J_RIGHT:
-            scroll_sprite(main_char.sprite_number, 10, 0);
-            //next_sprite(main_char, &current_sprite_index);
-            movement = 1;
-            delay(100);
-            break;
-        case J_UP:
-            scroll_sprite(main_char.sprite_number, 0, -10);
-            //next_sprite(main_char, &current_sprite_index);
-            movement = 1;
-            delay(100);
-            break;
-        case J_DOWN:
-            scroll_sprite(main_char.sprite_number, 0, 10);
-            //next_sprite(main_char, &current_sprite_index);
-            movement = 1;
-            delay(100);
-            break;
-        }
+        player_movement(&player);
     }
 }
